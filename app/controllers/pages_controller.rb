e41c7fc8882f450a060c8de266764f31f5205a81
markcyr <markcyr@msn.com>
2017-04-10 14:54:06 -0400

    routes controls contest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
 
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
   def welcome
     @header = "This is the welcome page"
@@ -7,15 +8,23 @@ class PagesController < ApplicationController
 
   def about
       @header = "This is the cool kids page"
-
-  end
+   end
 
   def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+  redirect_to "/"
 
-
+  end
 
 end
