92372e2a4164776d661c45e3f8e8fd672a20e318
markcyr <markcyr@msn.com>
2017-04-10 15:36:11 -0400

    routes controls secret

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,13 +18,23 @@ class PagesController < ApplicationController
 
   def set_kitten_url
     requested_size = params[:size]
+
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
   def contest
     flash[:notice] = "Sorry, the contest has ended"
   redirect_to "/"
+  end
 
+  def secret
+    secret_word = params[:magic_word]
+        if secret_word == 'password'
+      redirect_to '/secrets'
+        else
+          flash[:alert] =  "Sorry, you're not authorized to see that page!"
+          redirect_to "/"
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'pages#welcome'
 
 get '/' => 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
-get '/kitten/:size' => 'pages#kitten'
+get '/kitten/:size' => 'pages#kitten',  as: 'kitten'
 get '/kittens/:size' => 'pages#kittens'
+get '/secret/:magic_word' => 'pages#secret'
+get '/secrets' => 'pages#secrets'
+
 end
